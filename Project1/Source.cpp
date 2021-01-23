fd4a420738844f2a8756313a8efa0422ee706ad4
Nhat Tuan <nvntuan123@gmail.com>
2021-01-23 15:35:48 +0700

    edit project1

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,33 +1,65 @@
-#include <stdio.h>
-#include <conio.h>
+﻿#include <iostream>
+#include <vector>
+#include <functional> // Thư viện hỗ trợ sử dụng con trỏ hàm trong C++11
 
-#define NMAX 100
+using namespace std;
+
+void HoanVi_Pointer(int&, int&);
 
 int main()
 {
-	int A[NMAX];
+#ifndef Biến con trỏ hàm - Trong ngôn ngữ C hoặc C++98
+	void (*PtrFuncSwap)(int&, int&); // Khai báo con trỏ hàm.
+	PtrFuncSwap = HoanVi_Pointer;
 
-	int n, i, j;
+	int IValue1 = 5;
+	int IValue2 = 6;
 
-	printf_s("Nhap so luong phan tu cua day so, N = ");
-	scanf_s("%d", &n);
+	cout << "Truoc Khi Hoan Vi\n";
+	cout << "a = " << IValue1 << "\n";
+	cout << "b = " << IValue2 << "\n";
 
-	for (i = 0; i < n; i++)
-	{
-		printf_s("Phan tu thu: %d = ", i + 1);
-		scanf_s("%d", &A[i]);
-	}
+	//HoanVi_Pointer(IValue1, IValue2);
 
-	j = 1;
-	for (i = 1; i < n; i++)
-	{
-		if (A[i] > A[j])
-		{
-			j = i;
-		}
-	}
-	printf_s("Chi so: %d Gia tri: %d", j, A[j]);
+	// Gọi hàm 1 cách không tường mình.
+	//PtrFuncSwap(IValue1, IValue2);
 
-	_getch();
+	// Gọi hàm 1 cách tường minh.
+	(*PtrFuncSwap)(IValue1, IValue2);
+
+	cout << "\nSau Khi Hoan Vi\n";
+	cout << "a = " << IValue1 << "\n";
+	cout << "b = " << IValue2 << "\n";
+#endif // !Biến con trỏ hàm - Trong ngôn ngữ C hoặc C++98
+
+//#ifndef Biến con trỏ hàm - Trong ngôn ngữ C++ version 2011 trở lên
+//	vector<int> IArrVT; // Mảng 1 chiều.
+//	vector<vector<int>> IMatrixVT; // Mảng 2 chiều.
+//
+//	function<void(int&, int&)> PtrFunc = HoanVi_Pointer;
+//	//PtrFunc = HoanVi_Pointer;
+//
+//	int IValue1 = 5;
+//	int IValue2 = 6;
+//
+//	cout << "Truoc Khi Hoan Vi\n";
+//	cout << "a = " << IValue1 << "\n";
+//	cout << "b = " << IValue2 << "\n";
+//
+//	PtrFunc(IValue1, IValue2);
+//
+//	cout << "\nSau Khi Hoan Vi\n";
+//	cout << "a = " << IValue1 << "\n";
+//	cout << "b = " << IValue2 << "\n";
+//#endif // !Biến con trỏ hàm - Trong ngôn ngữ C++ version 2011 trở lên
+
+	system("pause");
 	return 0;
+}
+
+void HoanVi_Pointer(int& IA, int& IB)
+{
+	int ITemp = IA;
+	IA = IB;
+	IB = ITemp;
 }
